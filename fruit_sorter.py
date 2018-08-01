a23fc1766c9b1f9230e658a7e657369fc4fc2f1b
georgem20-meet <georgem20@meet.mit.edu>
2018-08-01 05:29:15 -0400

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,8 @@
+new_fruit = input('What fruit am I sorting?")
+if apples:
+    print('Bin 1')
+elif banana:
+    print('Bin 2')
+else:
+    print('Error! I do not recognise this fruit')
+                  
